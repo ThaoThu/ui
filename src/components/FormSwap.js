beb226c2839b1b2093153d2a14713ab4db9442d9
Rosa <thaothung.04@gmail.com>
2020-11-26 11:37:36 +0700

    update form

--- src/components/FormSwap.js
+++ src/components/FormSwap.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { Button, Col, Form, Input, notification, Row } from 'antd';
+import { Button, Col, Form, Input, Menu, Row, Dropdown } from 'antd';
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
 export default function FormSwap(props) {
     const FormItem = Form.Item;
     // const { getFieldDecorator } = props.form;
     let handleSubmit = () => {
 
     }
+    let handleMenuClick = () => { }
+
+    const menu = (
+        <Menu onClick={handleMenuClick}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+                1st menu item
+          </Menu.Item>
+            <Menu.Item key="2" icon={<UserOutlined />}>
+                2nd menu item
+          </Menu.Item>
+            <Menu.Item key="3" icon={<UserOutlined />}>
+                3rd menu item
+          </Menu.Item>
+        </Menu>
+    );
+
     return (
         <div className='form-swap'>
             <div className='form-swap__title'>Swap</div>
@@ -16,19 +33,32 @@ export default function FormSwap(props) {
                     />
 
                 </FormItem>
-                <FormItem label="Mua">
+                <div className='buy-box'>
 
-                    <Input
-                        type="number"
+                    <FormItem label="Mua" style={{ flexGrow: 1, marginRight: 5 }}>
 
-                    />
+                        <Input
+                            type="number"
 
-                    <small>
-                        Giá và số tiền sau cùng được xác định bởi số lượng token có trong pool tại thời điểm swap của bạn.
+                        />
+
+
+
+                    </FormItem>
+                    <FormItem label=" " >
+
+                        <Dropdown overlay={menu}>
+                            <Button>
+                                Button <DownOutlined />
+                            </Button>
+                        </Dropdown>
+                    </FormItem>
+
+                </div>
+                <small>
+                    Giá và số tiền sau cùng được xác định bởi số lượng token có trong pool tại thời điểm swap của bạn.
                 </small>
-                    <Button>Đăng nhập vào Swap</Button>
-
-                </FormItem>
+                <Button className='btn-submit'>Đăng nhập vào Swap</Button>
             </Form>
         </div>
     )
