8ad7fe4a2e99bc84485931d6cf522b8656833f52
Rosa <thaothung.04@gmail.com>
2020-11-26 14:39:21 +0700

    updated form

--- src/components/FormSwap.js
+++ src/components/FormSwap.js
@@ -1,66 +1,62 @@
-import React from 'react'
-import { Button, Col, Form, Input, Menu, Row, Dropdown } from 'antd';
-import { DownOutlined, UserOutlined } from '@ant-design/icons';
+import React, { useState } from "react";
+import { Button, InputNumber, Form, Input, Select } from "antd";
+import { SwapOutlined } from "@ant-design/icons";
 export default function FormSwap(props) {
-    const FormItem = Form.Item;
-    // const { getFieldDecorator } = props.form;
-    let handleSubmit = () => {
+  // const { getFieldDecorator } = props.form;
+  let handleSubmit = () => {};
+  const [value, setValue] = useState({
+    buyValue: "MCASH",
+    sellValue: "BTC",
+  });
 
-    }
-    let handleMenuClick = () => { }
+  const { Option } = Select;
 
-    const menu = (
-        <Menu onClick={handleMenuClick}>
-            <Menu.Item key="1" icon={<UserOutlined />}>
-                1st menu item
-          </Menu.Item>
-            <Menu.Item key="2" icon={<UserOutlined />}>
-                2nd menu item
-          </Menu.Item>
-            <Menu.Item key="3" icon={<UserOutlined />}>
-                3rd menu item
-          </Menu.Item>
-        </Menu>
-    );
-
-    return (
-        <div className='form-swap'>
-            <div className='form-swap__title'>Swap</div>
-            <Form onSubmit={handleSubmit} layout={'vertical'}>
-                <FormItem label="Bán">
-                    <Input
-                        type="number"
-                    />
-
-                </FormItem>
-                <div className='buy-box'>
-
-                    <FormItem label="Mua" style={{ flexGrow: 1, marginRight: 5 }}>
-
-                        <Input
-                            type="number"
-
-                        />
-
-
-
-                    </FormItem>
-                    <FormItem label=" " >
-
-                        <Dropdown overlay={menu}>
-                            <Button>
-                                Button <DownOutlined />
-                            </Button>
-                        </Dropdown>
-                    </FormItem>
-
-                </div>
-                <small>
-                    Giá và số tiền sau cùng được xác định bởi số lượng token có trong pool tại thời điểm swap của bạn.
-                </small>
-                <Button className='btn-submit'>Đăng nhập vào Swap</Button>
-            </Form>
+  return (
+    <div className="form-swap">
+      <div className="form-swap__title">Swap</div>
+      <Form onSubmit={handleSubmit} layout={"vertical"}>
+        <div className="group-item">
+          <label>Mua</label>
+          <Input.Group>
+            <Input type="number" />
+            <Select value={value.buyValue}>
+              <Option value="MCASH">MCASH</Option>
+              <Option value="BTC">BTC</Option>
+            </Select>
+          </Input.Group>
         </div>
-    )
+        <div className="text-center">
+          <span
+            className="swap-icon"
+            onClick={() => {
+              let { buyValue, sellValue } = value;
+              [buyValue, sellValue] = [sellValue, buyValue];
+              setValue({
+                buyValue,
+                sellValue,
+              });
+            }}
+          >
+            <SwapOutlined rotate={90} />
+          </span>
+        </div>
+        <div className="group-item">
+          <label>Ban</label>
+          <Input.Group>
+            <Input type="number" />
+            <Select value={value.sellValue}>
+              <Option value="MCASH">MCASH</Option>
+              <Option value="BTC">BTC</Option>
+            </Select>
+          </Input.Group>
+        </div>
+        <small>
+          Giá và số tiền sau cùng được xác định bởi số lượng token có trong pool
+          tại thời điểm swap của bạn.
+        </small>
+        <Button className="btn-submit">Đăng nhập vào Swap</Button>
+      </Form>
+    </div>
+  );
 }
 // export default Form.create({ name: 'FormSwap' })(FormSwap);
